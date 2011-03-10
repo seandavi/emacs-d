1c95b9ab24771061ab9b9bc16f65d5c9d9c067bd
Sean Davis <seandavi@gmail.com>
2011-03-10 08:16:59 -0500

    working ropemacs, python completion (partial), and yasnippets

---
+++ init.el
@@ -0,0 +1,34 @@
+(add-to-list 'load-path "~/.emacs.d/site-lisp/")
+(add-to-list 'load-path "~/.emacs.d/site-lisp/auto-complete-1.3.1")
+(autoload 'python-mode "python-mode" "Python Mode." t)
+(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+(add-to-list 'interpreter-mode-alist '("python" . python-mode))
+(require 'python-mode)
+(autoload 'pymacs-apply "pymacs")
+(autoload 'pymacs-call "pymacs")
+(autoload 'pymacs-eval "pymacs" nil t)
+(autoload 'pymacs-exec "pymacs" nil t)
+(autoload 'pymacs-load "pymacs" nil t)
+(pymacs-load "ropemacs" "rope-")
+(setq ropemacs-enable-autoimport t)
+
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "/Users/sdavis/.emacs.d/site-lisp/ac-dict")
+(ac-config-default)
+(global-auto-complete-mode t)
+
+(setq auto-mode-alist (cons '("\\.py$" . python-mode) auto-mode-alist))
+(setq interpreter-mode-alist (cons '("python" . python-mode) interpreter-mode-alist))
+(autoload 'python-mode "python-mode" "Python editing mode." t)
+(require 'pymacs)
+(pymacs-load "ropemacs" "rope-")
+
+;; yasnippet bundle load
+(require 'yasnippet-bundle)
+
+;; yasnippet normal install
+(add-to-list 'load-path
+	     "~/.emacs.d/site-lisp/yasnippet-0.6.1c")
+    (require 'yasnippet) ;; not yasnippet-bundle
+    (yas/initialize)
+    (yas/load-directory "~/.emacs.d/site-lisp/yasnippet-0.6.1c/snippets")
