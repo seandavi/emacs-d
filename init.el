7d81d795806af1181f10ae910c99d711ac1f7bd3
Sean Davis <seandavi@gmail.com>
2012-01-24 12:44:07 -0500

    Changed color theme to arjen

--- init.el
+++ init.el
@@ -48,7 +48,7 @@
 (eval-after-load "color-theme"
   '(progn
      (color-theme-initialize)
-     (color-theme-gnome)))
+     (color-theme-arjen)))
 
 ;; ESS
 (add-to-list 'load-path "~/.emacs.d/site-lisp/ess-5.14/lisp")
@@ -69,7 +69,8 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t))
+ '(inhibit-startup-screen t)
+ '(tool-bar-mode nil))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
